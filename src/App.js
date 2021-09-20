4b6a2a935cfdc8aaf52de1b4089945c020f78c4c
Danulo004 <danulo004@gmail>
2021-09-20 12:59:31 +0300

    Added mvc pzttern

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import ClickController from "./controlers/ClickController";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
+
+  const [disableButton, setDisableButton] = useState(false);
+
+  const onClick = async () => {
+    setDisableButton(true);
+    const newPhotoUrl = await ClickController.onGetPhotoByKeywordsClick(input);
+
+    setPhotoUrl(newPhotoUrl);
+
+    setDisableButton(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js </code> and save to reload.
-        </p>
         <p>Hello from Danylo</p>
         <p>On Heroku</p>
+        <hr className="hr" />
+
+        <label>Enter Photo aliases:</label>
+        <input
+          name="fname"
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={onClick} disabled={disableButton}>
+          Get photos
+        </button>
+
+        <hr className="hr" />
+
+        <img src={photoUrl} width="80%" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
